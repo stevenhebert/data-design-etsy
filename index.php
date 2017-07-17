1fec191dbffacd5811cb242a92460686fe405fff
steven hebert <shebert2@cnm.edu>
2017-07-17 02:22:22 -0600

    created index

--- index.php
+++ index.php
@@ -7,41 +7,41 @@
 
 		<body>
 
-			<h1>purpose, audience, & goal</h1> This project is a retail platform focused on the retailers experience. The
+			<h1>Purpose, Audience, Goal:</h1> This project is a retail platform focused on the retailers experience. The
 			site will offer a venue for sellers of unique goods to connect with their niche buyers. The goal of the project
 			is to establish a retail-home catered to the craft enthusiast. The typical seller is not soley interested in
 			profit, nor is the typical buyer necessarily interested in the best value.
 
 
 
-			<h1>persona</h1>
+			<h1>Persona:</h1>
 
-			<h2>profession</h2> Seller profile for producer of hair-ables <em>"hair-wearables"</em> made from donated canine
+			<h2>Profession</h2> Seller profile for producer of hair-ables <em>"hair-wearables"</em> made from donated canine
 			trimmings. this unique individual is is an ethical purveyor of cosmetic and beauty-enhancing items. seller is
 			adamant about conveying this information to buyers.
 
-			<h2>technology</h2> Although an expert in her field, user is technologically inept; she's easily frustrated by
+			<h2>Technology</h2> Although an expert in her field, user is technologically inept; she's easily frustrated by
 			technological barriers. Is able to navigate a computer with little assistance and is capable of
 			recording/capturing media with her mobile device.
 
-			<h2>attitude/behavior</h2> Seller has an open schedule which she prefers not to spend wrestling with computer
+			<h2>Attitude/Behavior</h2> Seller has an open schedule which she prefers not to spend wrestling with computer
 			problems.
 
-			<h2>frustration/needs</h2> Seller needs a platform that is easy to navigate and provides easy access
+			<h2>Frustration/Needs</h2> Seller needs a platform that is easy to navigate and provides easy access
 			to technical support. The seller desires a store front that is polished and showcases her items. Item listings
 			should offer a variety of ways to convey product information, production details and materials, and shipping
 			and return policies. The platform should be accessible to an international audience, accept a variety of
 			payment options, as well as provide performance metrics.
 
-			<h2>goals</h2> The seller needs an incentive to choose this site over competing platforms (e.g., eBay, Amazon).
+			<h2>Goals</h2> The seller needs an incentive to choose this site over competing platforms (e.g., eBay, Amazon).
 			She needs a retail venue oriented towards custom, one-off goods, not mass-produced goods. This should be the
 			first destination both buyers and sellers think of for this type of item.
 
-			<h2>user story</h2> As a retailer, I need a no-hassle experience to market my goods to an international audience.
+			<h2>User Story</h2> As a retailer, I need a no-hassle experience to market my goods to an international audience.
 
 
 
-			<h1>use case:</h1> A number of buyers have sent payments for the sellers items. A handful of potential buyers
+			<h1>Use Case:</h1> A number of buyers have sent payments for the sellers items. A handful of potential buyers
 			have inquired about the specifics regarding the sellers items.
 
 			The seller prepares her orders for pickup by USPS. After packaging the items she updates the status of her
@@ -53,12 +53,12 @@
 
 
 
-			<h1>interaction flow</h1>
+			<h1>Interaction Flow:</h1>
 
-			<h2>profile creation</h2> Seller must first register a profile; the individual may use a linked service
+			<h2>Profile Creation</h2> Seller must first register a profile; the individual may use a linked service
 			that can provide registration information.
 
-			<h2>shop creation</h2> Seller chooses a shop name.
+			<h2>Shop Creation:</h2> Seller chooses a shop name.
 
 			<h2>item listing</h2> After the shop is established, the interface provides fields for
 			the seller to specify item category, search tags, item description, production process/materials,
@@ -66,7 +66,7 @@
 
 
 
-			<h1>conceptual model</h1>
+			<h1>Conceptual Model:</h1>
 			<ul>
 				<li></li>
 				<li></li>
@@ -74,18 +74,5 @@
 			</ul>
 
 		</body>
-
-
-
-
-
-
-
-
-
-
-
-
-
 </html>
 
