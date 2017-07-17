9e71d29d7d8bdfe640c43eba9ca8b9eecf99a6ac
steven hebert <shebert2@cnm.edu>
2017-07-17 01:49:28 -0600

    created index

--- index.php
+++ index.php
@@ -1,1 +1,79 @@
-hi
+<!DOCTYPE html>
+<meta charset="UTF-8">
+
+<html lang="en">
+
+	<title>Data Design Etsy</title>
+
+		<body>
+
+			<h1>purpose, audience, & goal</h1> This project is a retail platform focused on the retailers experience. The
+			site will offer a venue for sellers of unique goods to connect with their niche buyers. The goal of the project
+			is to establish a retail-home catered to the craft enthusiast. The typical seller is not soley interested in
+			profit, nor is the typical buyer necessarily interested in the best value.
+
+			<h1>persona</h1>
+
+			<h2>profession</h2> Seller profile for producer of hair-ables <em>"hair-wearables"</em> made from donated canine
+			trimmings. this unique individual is is an ethical purveyor of cosmetic and beauty-enhancing items. seller is
+			adamant about conveying this information to buyers.
+
+			<h2>technology</h2> Although an expert in her field, user is technologically inept; she's easily frustrated by
+			technological barriers. Is able to navigate a computer with little assistance and is capable of
+			recording/capturing media with her mobile device.
+
+			<h2>attitude/behavior</h2> Seller has an open schedule which she prefers not to spend wrestling with computer
+			problems.
+
+			<h2>frustration/needs</h2> Seller needs a platform that is easy to navigate and provides easy access
+			to technical support. The seller desires a store front that is polished and showcases her items. Item listings
+			should offer a variety of ways to convey product information, production details and materials, and shipping
+			and return policies. The platform should be accessible to an international audience, accept a variety of
+			payment options, as well as provide performance metrics.
+
+			<h2>goals</h2> The seller needs incentive to choose this site over competing platforms (e.g., eBay, Amazon).
+			She needs a retail venue oriented customized, one-off goods, not mass-produced goods. This should be the first
+			destination both sellers and buyers think of when searching for these types of crafts and services.
+
+			<h2>profile creation</h2> Seller must first register a profile; the individual may use a linked service
+			that can provide registration information.
+			<h2>shop creation:</h2> Seller chooses a shop name. Once the shop is established interfae provides fields for
+			the seller to specify item category, search tags, describe the item, production process/materials,
+			provide photos, list quantity offered, customization options, shipping and return policies.
+
+
+			<h1>use case:</h1> The seller has exhausted her local audience and desires to expand her sales through
+			international exposure. The seller desires to in a niche industry and is una. the ideal platform offers sellers a simplistic interface focused
+			on the sellers business needs. the user experience should be easy to navigate and provide a range of
+			customization options; tips and guides should be visable but non-intrusive.
+
+			<h1>interaction flow</h1>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+
+			<h1>conceptual model</h1>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+
+		</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
+
