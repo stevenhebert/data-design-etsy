d60622aa278775bbe7be4c3d2d17cf85257bc22f
steven hebert <shebert2@cnm.edu>
2017-07-24 16:55:25 -0600

    fixed database, tables working, data inserted added php dir, started docs

---
+++ php/classes/Item.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: steven
+ * Date: 7/24/17
+ * Time: 4:43 PM
+ */
+
+class Item {
+
+}
---
+++ php/classes/Profile.php
@@ -0,0 +1,13 @@
+<?php
+
+/**php practice coding a simplified etsy user profile
+ * this demonstrates one method you could use to record and retrieve end user data into an online-retailing database
+ * the hierarchy could be extended or modified for different platforms/realism
+ *
+ * @author Steven Hebert
+ * @version 1.0.1
+ **/
+
+class Profile {
+
+}
