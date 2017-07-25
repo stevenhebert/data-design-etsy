ce0727cc9db42994487725382838e074f3bcda10
steven <shebert2@cnm.edu>
2017-07-24 23:13:42 -0600

    fixed database, tables working, data inserted added php dir, started docs

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -1,13 +1,19 @@
 <?php
 
-/**php practice coding a simplified etsy user profile
+/**
+ * php practice coding a simplified etsy user profile
+ *
  * this demonstrates one method you could use to record and retrieve end user data into an online-retailing database
  * the hierarchy could be extended or modified for different platforms/realism
  *
- * @author Steven Hebert
+ * @author Steven Hebert <shebert2@cnm.edu>
  * @version 1.0.1
  **/
 
 class Profile {
+	/**
+	 * this is the profile id, here profile id is assigned as the primary key
+	 */
+	private profileId
 
 }
