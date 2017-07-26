0cb6f64d07417ffcb381aa0e6a4b89b3687d04e1
steven <shebert2@cnm.edu>
2017-07-26 10:08:33 -0600

    command shift f'd

--- php/classes/Items.php
+++ php/classes/Items.php
@@ -18,7 +18,7 @@ class items {
 
 	/**
 	 * this forms the basis for the items foreign key, will ultimately be used to keep track of whose items are whose
-	*/
+	 */
 	private $itemProfileId;
 
 	/**
@@ -35,6 +35,64 @@ class items {
 	 * description of the users item
 	 */
 	private $itemDes;
+}
+
+
+/**
+ * accessor method for itemId
+ *
+ * @return int value of itemId
+ **/
+public
+function itemId(): int {
+	return $this->itemId;
+}
+
+/**
+ * mutator method for itemId
+ *
+ * @param int $newItemId new value of itemId
+ * @throws \UnexpectedValueException if $newItemId is not an integer
+ **/
+public
+function setItemId(?int $newItemId): void {
+	if($newItemId === null) {
+		$this->itemeId = null;
+		return;
+	}
+
+	//verify itemId is positive
+	if($newItemId <= 0) {
+		throw(new \RangeException("itemId is not positive"));
+	}
+
+	//convert and store the itemId (saving work)
+	$this->itemId = intval($newItemId);
+}
+
+
+/**
+ * mutator method for itemProfileId
+ *
+ * @param int $newitemProfileId new value of itemProfileId
+ * @throws \UnexpectedValueException if $newItemProfileId is not an integer
+ **/
+public
+function setItemProfileId(?int $newItemProfileId): void {
+	if($newItemProfileId === null) {
+		$this->itemProfileId = null;
+		return;
+	}
+
+	//verify itemProfileId is positive
+	if($newItemProfileId <= 0) {
+		throw(new \RangeException("itemProfileId is not positive"));
+	}
+
+	//convert and store the itemProfileId (saving work)
+	$this->itemProfileId = intval($newItemProfileId);
+}
+
 
 }
 ?>
